4df31f486fc0c55ec5adf1e92b7f2d3a26eb2164
Kavia AI <production@kavia.ai>
2025-09-17 13:56:52 +0000

    CheckPoint - cgc807aab4

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/simple-to-do-app-12288-12300/to_do_frontend
+flutter analyze
+
---
+++ to_do_frontend/android/app/src/main/kotlin/com/example/to_do_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.to_do_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
